5ddd322509b6b8079f43f6c28a0b6e35422b7751
bytekarmaa <161755557+bytekarmaa@users.noreply.github.com>
2026-09-04 02:25:29 +0530

    Time: 347 ms (5.01%), Space: 340.8 MB (5.11%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -3,55 +3,117 @@
 // by pure backtracking
 
 
+// class Solution {
+// public:
+//     int n;
+    
+//     bool isPalindrome(string &s, int l, int r) {
+        
+//         while(l < r) {
+//             if(s[l] != s[r])
+//                 return false;
+//             l++;
+//             r--;
+//         }
+        
+//         return true;
+        
+//     }
+    
+//     void backtrack(string &s, int idx, vector<string> &curr, vector<vector<string>> &result) {
+        
+//         if(idx == n) {
+//             result.push_back(curr);
+//             return;
+//         }
+        
+        
+//         for(int i = idx; i<n; i++) {
+            
+//             if(isPalindrome(s, idx, i)) {
+                
+//                 curr.push_back(s.substr(idx, i-idx+1));
+                
+//                 backtrack(s, i+1, curr, result);
+                
+//                 curr.pop_back();
+                
+//             }
+            
+//         }
+        
+//     }
+    
+//     vector<vector<string>> partition(string s) {
+//         n = s.length();
+//         vector<vector<string>> result;
+//         vector<string> curr;
+        
+//         backtrack(s, 0, curr, result);
+        
+//         return result;
+        
+//     }
+// };
+
+
+
+
+// dp approch
+
 class Solution {
 public:
-    int n;
-    
-    bool isPalindrome(string &s, int l, int r) {
-        
-        while(l < r) {
-            if(s[l] != s[r])
-                return false;
-            l++;
-            r--;
-        }
-        
-        return true;
-        
+vector<vector<bool>> dp;
+
+vector<vector<string>> result;
+
+void solve(string &s, int i, int j, vector<string> ref,int &n){
+   
+    if(i == n){
+        result.push_back(ref);
+        return;
     }
-    
-    void backtrack(string &s, int idx, vector<string> &curr, vector<vector<string>> &result) {
-        
-        if(idx == n) {
-            result.push_back(curr);
-            return;
-        }
-        
-        
-        for(int i = idx; i<n; i++) {
-            
-            if(isPalindrome(s, idx, i)) {
-                
-                curr.push_back(s.substr(idx, i-idx+1));
-                
-                backtrack(s, i+1, curr, result);
-                
-                curr.pop_back();
-                
-            }
-            
-        }
-        
+
+     if(j == n) return;
+
+    if(dp[i][j]){
+        ref.push_back(s.substr(i,j-i+1));
+        solve(s,j+1,j+1,ref,n);
+        ref.pop_back();
     }
-    
+
+    solve(s,i,j+1,ref,n);
+}
+
     vector<vector<string>> partition(string s) {
-        n = s.length();
-        vector<vector<string>> result;
-        vector<string> curr;
-        
-        backtrack(s, 0, curr, result);
-        
+        int n = s.size();
+
+        dp.resize(n,vector<bool> (n,false));
+
+        for(int len = 1; len <= n; len++){
+            for(int i = 0; i < n - len + 1; i++){
+                int j = len + i - 1;
+
+                if(i == j) dp[i][j] = true;
+
+                else if(s[i] == s[j]){
+                    if(len == 2) dp[i][j] = true;
+
+                    else dp[i][j] = dp[i+1][j-1];
+                }
+            }
+        }
+
+        vector<string> ref;
+
+        solve(s,0,0,ref,n);
+
         return result;
-        
+
     }
 };
+
+
+
+
+
