708eb0207b8d34e0e6fb7570b9668d0db2a647bf
bytekarmaa <161755557+bytekarmaa@users.noreply.github.com>
2026-09-06 02:43:55 +0530

    Time: 0 ms (100%), Space: 13.2 MB (21.31%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,53 @@
+// class Solution {
+// public:
+//     string findDifferentBinaryString(vector<string>& nums) {
+//         int n = nums.size();
+
+//         vector<int> bit_freq
+//     }
+// };
+
+
+
+
+
+// staright forward backtracking
+
+
+class Solution {
+public:
+
+unordered_map<string,int> mp;
+
+bool find(string ref, string &res,int n){
+    if(ref.size() == n){
+        if(!mp.count(ref)){
+            res = ref;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool x = find(ref+'0',res,n);
+
+    if(x) return x;
+
+    bool y = find(ref + '1',res,n);
+
+    if(y) return y;
+
+    return x || y;
+    
+}
+
+    string findDifferentBinaryString(vector<string>& nums) {
+        for(int i = 0; i < nums.size(); i++) mp[nums[i]];
+         string res = "";
+
+      find("",res, nums.size());
+
+      return res;
+    }
+
+};
