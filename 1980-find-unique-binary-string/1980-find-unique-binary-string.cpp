d18df433d5afa692229de94cf48afff22f1bb135
bytekarmaa <161755557+bytekarmaa@users.noreply.github.com>
2026-09-06 02:49:29 +0530

    Time: 0 ms (100%), Space: 12.3 MB (71.79%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,11 +1,17 @@
-// class Solution {
-// public:
-//     string findDifferentBinaryString(vector<string>& nums) {
-//         int n = nums.size();
-
-//         vector<int> bit_freq
-//     }
-// };
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string ans = "";
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i][i] == '0') {
+                ans += '1';
+            } else {
+                ans += '0';
+            }
+        }
+        return ans;
+    }
+};
 
 
 
@@ -14,40 +20,40 @@
 // staright forward backtracking
 
 
-class Solution {
-public:
+// class Solution {
+// public:
 
-unordered_map<string,int> mp;
+// unordered_map<string,int> mp;
 
-bool find(string ref, string &res,int n){
-    if(ref.size() == n){
-        if(!mp.count(ref)){
-            res = ref;
-            return true;
-        }
+// bool find(string ref, string &res,int n){
+//     if(ref.size() == n){
+//         if(!mp.count(ref)){
+//             res = ref;
+//             return true;
+//         }
 
-        return false;
-    }
+//         return false;
+//     }
 
-    bool x = find(ref+'0',res,n);
+//     bool x = find(ref+'0',res,n);
 
-    if(x) return x;
+//     if(x) return x;
 
-    bool y = find(ref + '1',res,n);
+//     bool y = find(ref + '1',res,n);
 
-    if(y) return y;
+//     if(y) return y;
 
-    return x || y;
+//     return x || y;
     
-}
+// }
 
-    string findDifferentBinaryString(vector<string>& nums) {
-        for(int i = 0; i < nums.size(); i++) mp[nums[i]];
-         string res = "";
+//     string findDifferentBinaryString(vector<string>& nums) {
+//         for(int i = 0; i < nums.size(); i++) mp[nums[i]];
+//          string res = "";
 
-      find("",res, nums.size());
+//       find("",res, nums.size());
 
-      return res;
-    }
+//       return res;
+//     }
 
-};
+// };
