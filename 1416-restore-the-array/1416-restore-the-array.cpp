bc95bd1c5e04d54d6423ee37a1bb8f4a1ca46a88
bytekarmaa <161755557+bytekarmaa@users.noreply.github.com>
2026-09-23 01:44:48 +0530

    Time: 32 ms (53.32%), Space: 15.4 MB (89.72%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -1,39 +1,66 @@
 class Solution {
 public:
-
-int n;
-
-vector<int> dp;
-
-int mod = 1e9 + 7;
-
-int solve(string &s, int &k , int ind){
-    if(ind == n) return 1;
-
-    if(s[ind] == '0') return 0; // as we cannot split with leading zeroes
-
-    if(dp[ind] != -1) return dp[ind];
-
-    long long num = 0;
-
-    int ways = 0;
-
-    for(int i = ind; i < n; i++){
-        num = num * 10 + (s[i] - '0');
-
-        if(num > k) break;
-
-        ways = (ways % mod + solve(s,k,i+1) % mod) % mod;
-    }
-
-    return dp[ind] = ways;
-}
-
     int numberOfArrays(string s, int k) {
-        n = s.size();
+        int n = s.size();
+        vector<int> dp(n+1,0);
+        dp[n] = 1;
 
-        dp.assign(n,-1);
+        int mod = 1e9 + 7;
 
-        return solve(s,k,0);
+        for(int i = n-1; i >= 0; i--){
+            if(s[i] == '0') continue;
+
+            long long num = 0;
+
+            for(int j = i; j < n; j++){
+                 num = num * 10 + (s[j] - '0');
+
+                 if(num > k) break;
+
+                dp[i] = (dp[i] % mod + dp[j+1] % mod) % mod;
+            }
+        }
+
+        return dp[0];
     }
 };
+
+// class Solution {
+// public:
+
+// int n;
+
+// vector<int> dp;
+
+// int mod = 1e9 + 7;
+
+// int solve(string &s, int &k , int ind){
+//     if(ind == n) return 1;
+
+//     if(s[ind] == '0') return 0; // as we cannot split with leading zeroes
+
+//     if(dp[ind] != -1) return dp[ind];
+
+//     long long num = 0;
+
+//     int ways = 0;
+
+//     for(int i = ind; i < n; i++){
+//         num = num * 10 + (s[i] - '0');
+
+//         if(num > k) break;
+
+//         ways = (ways % mod + solve(s,k,i+1) % mod) % mod;
+//     }
+
+//     return dp[ind] = ways;
+// }
+
+//     int numberOfArrays(string s, int k) {
+//         n = s.size();
+
+//         dp.assign(n,-1);
+
+//         return solve(s,k,0);
+//     }
+// };
